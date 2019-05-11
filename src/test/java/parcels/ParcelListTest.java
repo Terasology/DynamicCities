cb20410888f25cd31539e0620139967faa58ac06
Mayant Mukul <mayantmukul@hotmail.com>
2019-05-11 16:11:20 +0530

    Fix ParcelListTest

--- src/test/java/parcels/ParcelListTest.java
+++ src/test/java/parcels/ParcelListTest.java
@@ -15,11 +15,27 @@
  */
 package parcels;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.terasology.commonworld.Orientation;
+import org.terasology.dynamicCities.parcels.DynParcel;
+import org.terasology.dynamicCities.parcels.ParcelList;
+import org.terasology.math.geom.Rect2i;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ParcelListTest {
-/*
     public ParcelList parcels;
     public Rect2i[] shapes;
+
+    final String CLERICAL = "CLERICAL";
+    final String RESIDENTIAL = "RESIDENTIAL";
+    final String GOVERNMENTAL = "GOVERNMENTAL";
+
     @Before
     public void setup() {
         parcels = new ParcelList(1);
@@ -29,22 +45,22 @@ public class ParcelListTest {
         shapes[2] = Rect2i.createFromMinAndSize(-13, -13, 10, 10);
         shapes[3] = Rect2i.createFromMinAndSize(-5, -5, 100, 100);
         shapes[4] = Rect2i.createFromMinAndSize(-100, -100, 10, 10);
-        parcels.addParcel(new DynParcel(shapes[0], Orientation.EAST, Zone.CLERICAL, 0));
-        parcels.addParcel(new DynParcel(shapes[1], Orientation.EAST, Zone.RESIDENTIAL, 0));
-        parcels.addParcel(new DynParcel(shapes[2], Orientation.EAST, Zone.GOVERNMENTAL, 0));
+        parcels.addParcel(new DynParcel(shapes[0], Orientation.EAST, CLERICAL, 0));
+        parcels.addParcel(new DynParcel(shapes[1], Orientation.EAST, RESIDENTIAL, 0));
+        parcels.addParcel(new DynParcel(shapes[2], Orientation.EAST, GOVERNMENTAL, 0));
     }
 
     @Test
     public void testIntersection() {
-        assertEquals(true, parcels.isNotIntersecting(shapes[4]));
-        assertEquals(false, parcels.isNotIntersecting(shapes[3]));
+        assertTrue(parcels.isNotIntersecting(shapes[4]));
+        assertFalse(parcels.isNotIntersecting(shapes[3]));
     }
 
     @Test
     public void testZoneArea() {
-        assertEquals(100, parcels.clericalArea);
-        assertEquals(100, parcels.residentialArea);
-        assertEquals(100, parcels.governmentalArea);
+        Map<String, Integer> areas = parcels.areaPerZone;
+        assertEquals(100, (long) areas.get(CLERICAL));
+        assertEquals(100, (long) areas.get(RESIDENTIAL));
+        assertEquals(100, (long) areas.get(GOVERNMENTAL));
     }
-    */
 }
