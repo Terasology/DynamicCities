3a17c721a6381d5412b3eef6d127b85b4d8ef3d6
Tobias Nett <skaldarnar@googlemail.com>
2021-08-13 19:04:24 +0200

    feat: make ResourceProvider scalable

--- src/main/java/org/terasology/dynamicCities/region/ResourceProvider.java
+++ src/main/java/org/terasology/dynamicCities/region/ResourceProvider.java
@@ -1,51 +1,36 @@
-/*
- * Copyright 2016 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.dynamicCities.region;
 
 import org.terasology.dynamicCities.facets.ResourceFacet;
 import org.terasology.dynamicCities.facets.RoughnessFacet;
 import org.terasology.engine.world.generation.Border3D;
-import org.terasology.engine.world.generation.FacetProvider;
 import org.terasology.engine.world.generation.GeneratingRegion;
 import org.terasology.engine.world.generation.Produces;
+import org.terasology.engine.world.generation.ScalableFacetProvider;
 import org.terasology.engine.world.generator.plugin.RegisterPlugin;
+import org.terasology.math.TeraMath;
 
+/**
+ * This facet will be used to store information about the resources in its region.
+ *
+ * The actual data will be set through CompatibleRasterizer. It will be saved as a Component in the RegionEntity.
+ */
 @RegisterPlugin
 @Produces(ResourceFacet.class)
-/**
- * This facet will be used to store information about the resources in its region
- * The actual data will be set through CompatibleRasterizer
- * It will be saved as a Component in the RegionEntity
- */
-public class ResourceProvider implements FacetProvider {
+public class ResourceProvider implements ScalableFacetProvider {
 
     private final int gridSize = 32;
 
+    @Override
+    public void setSeed(long seed) {
+    }
 
     @Override
-    public void setSeed(long seed) { }
-
-    @Override
-    public void process(GeneratingRegion region) {
-
+    public void process(GeneratingRegion region, float scale) {
         Border3D border = region.getBorderForFacet(RoughnessFacet.class);
-        ResourceFacet facet = new ResourceFacet(region.getRegion(), border, gridSize);
+        ResourceFacet facet = new ResourceFacet(region.getRegion(), border, TeraMath.ceilToInt(gridSize / scale));
 
         region.setRegionFacet(ResourceFacet.class, facet);
     }
-
-
 }
